23521a48b31a192b3a487e81a9beebd2b01beef7
Kepoulpe <moreno.n@hotmail.fr>
2022-03-17 19:28:46 +0100

    edit scripts for quantity and delete items

--- front/js/cart.js
+++ front/js/cart.js
@@ -7,7 +7,7 @@ const displayItems = (items) => {
     console.log(item);
 
      // modify DOM element to create an item card
-    const itemToDisplay = `<article class="cart__item" data-id="${item.product._id}" data-color="${item.color}">
+    const itemToDisplay = `<article class="cart__item">
       <div class="cart__item__img">
         <img src="${item.product.imageUrl}" alt="${item.product.altText}">
       </div>
@@ -20,18 +20,18 @@ const displayItems = (items) => {
         <div class="cart__item__content__settings">
           <div class="cart__item__content__settings__quantity">
             <p>Qté : ${item.quantity} </p>
-            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="">
+            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.quantity}" data-id="${item.product._id}" data-color="${item.color}">
           </div>
           <div class="cart__item__content__settings__delete">
-            <p class="deleteItem">Supprimer</p>
+            <p class="deleteItem" data-id="${item.product._id}" data-color="${item.color}">Supprimer</p>
           </div>
         </div>
       </div>
     </article>`;
 
     // we get the delete button
-    // id="delete_${item.product._id}" 415b7cacb65d43b2b5c1ff70f3393ad1
-    // document.querySelector("article[data-id='415b7cacb65d43b2b5c1ff70f3393ad1'] .deleteItem");
+    
+
     // we apply a click event on this delete button
 
     // this click event should trigger a behavior (function) that actually deletes the item
@@ -41,23 +41,58 @@ const displayItems = (items) => {
   });
 }
 
-// TODO delete an item 
-// const deleteProduct = async (items) => {
-//   // wait for the cartProducts function
-//   await cartProducts;
-//   // get all delete button
-//   const deleteButton = document.querySelectorAll(".deleteItem") 
-//   //get the parent element of the buttont to get data id and data color
-//   const deleteButtonParent = document.querySelector(".cart__item")
-//   console.log(deleteButton)
-//   deleteButton.forEach((button) => {
+// fucntion that will delete an item 
+const deleteItems = async (displayItems) => {
 
-//   button.addEventListener("click", () => {
-//     console.log(deleteButtonParent)
-//     // if products is empty delete local storage
-//   })
-//   })
-// }
+  // waiting for the items display 
+  await displayItems
+ 
+  // query all the buttons display dynamicly buy the previous function
+  const deleteButtons = document.querySelectorAll(".deleteItem")
+
+  // loop in the NodeList to get a specific button by clicking on
+  deleteButtons.forEach((button) =>{
+    button.addEventListener("click", (cartItems) => {
+    
+      //compare if data are equals
+      for (i=0; i< cartItems.lenght; i++) {
+
+        if (cartItems[i].product._id ==  button.dataset.id && cartItems[i].color == button.dataset.color) {
+
+          // if data is equal remove this specific item from local storage
+          return localStorage.removeItem(cartItems[i])
+        }
+      }
+    })
+  })
+}
+
+// function that will change the quantity of product
+const editQuantity = async (displayItems) => {
+
+  // waiting for the items display
+  await displayItems
+
+  // query all the inputs 
+  const inputsQuantity = document.querySelectorAll(".itemQuantity")
+  console.log(inputsQuantity)
+
+  // loop in the NodeList to get a specific input  and listen is onchange event
+  inputsQuantity.forEach((input) => {
+    input.addEventListener("change" ,() => {
+      
+      // compare if data are equals
+
+      for(i=[0]; i<cartItems.lenght; i++) {
+        if(cartItems[i].product._id == input.dataset.id && cartItems[i].color == input.dataset.color) {
+          return console.log(input.value)
+        }
+      }
+    })
+  })
+}
+
+
 
 // get all the prices values and multiply them with quantities to had a total price
 const priceItemsSum = (items) => {
@@ -65,6 +100,7 @@ const priceItemsSum = (items) => {
   return items.map(item => ({price: item.quantity * item.product.price}));
 
   /**
+   *  We can also do like this to display the total products price
    * 
     // declare an empty array of Items total price
     let itemsTotalPrice = [];
@@ -93,4 +129,7 @@ window.addEventListener('DOMContentLoaded', async (event) => {
   document.getElementById("totalQuantity").innerText = cartItems.map(item => item.quantity).reduce((prev, curr) => prev + curr, 0);
 
   displayItems(cartItems);
+  deleteItems(cartItems);
+  editQuantity(cartItems);
+  console.log(cartItems)
 });
